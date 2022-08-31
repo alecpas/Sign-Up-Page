7a14ad66f16895410bce67004240050f46c42cb5
alecpas <alec.buta@gmail.com>
2022-08-31 21:44:14 +0300

    excluded email validation regex

--- function.js
+++ function.js
@@ -2,18 +2,6 @@ const button = document.getElementById("button");
 const fname = document.getElementById("fname");
 const email = document.getElementById("email")
 
-const validateEmail = (input) => {
-
-    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  
-    if (input.value.match(validRegex)) {
-  
-      return true;}
-    
-      else {return false;}
-
-}
-
 button.onclick = () => {
     if (fname != null && validateEmail(email) === true) {
       alert('All credentials are correct!');
